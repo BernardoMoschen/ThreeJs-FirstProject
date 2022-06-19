0aa47ce13f7548852ab9520c485ea4c31e6d2016
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-06-19 14:16:03 -0300

    ref: rotations

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
+
+// Setup
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGL1Renderer({
@@ -14,6 +16,9 @@ camera.position.setZ(30)
 
 renderer.render(scene, camera)
 
+
+// Saturn Ring
+
 const geometry = new THREE.TorusGeometry(8, 2, 2, 100)
 const saturnRingTexture = new THREE.TextureLoader().load('./assets/saturnRingTexture.jpeg')
 const material = new THREE.MeshStandardMaterial({ map: saturnRingTexture} )
@@ -22,17 +27,24 @@ saturnRing.rotation.x += 5
 
 scene.add(saturnRing)
 
+
+// Lights
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(5,5,5)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 
+
+// Helpers
+
 // const lightHelper = new THREE.PointLightHelper(pointLight)
 // const gridHelper = new THREE.GridHelper(200, 50)
 // scene.add(lightHelper, gridHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+// const controls = new OrbitControls(camera, renderer.domElement)
 
+
+// Stars
 
 function addStar() {
     const geometry = new THREE.SphereGeometry(0.25, 24, 24)
@@ -49,8 +61,12 @@ function addStar() {
 
 Array(200).fill().forEach(addStar)
 
+
+//Background
+
 const spaceTexture = new THREE.TextureLoader().load('./assets/space.jpg')
 scene.background = spaceTexture
+
 
 // Saturn
 
@@ -64,37 +80,59 @@ const saturn = new THREE.Mesh(
 })
 )
 saturn.rotation.x += 0.5
-
 scene.add(saturn)
 
 
-function moveCamera () {
-    const t = document.body.getBoundingClientRect().top
+// Moon
 
-    saturn.rotation.x += 0.05
-    saturn.rotation.y += 0.075
-    saturn.rotation.z += 0.05
+const moonTexture = new THREE.TextureLoader().load('./assets/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('./assets/normal.jpg');
 
-    saturnRing.rotation.x += 0.05
-    saturnRing.rotation.y += 0.075
-    saturnRing.rotation.z += 0.05
-    
-    camera.rotation.x = t * -0.05
-    camera.rotation.y = t * -0.075
-    camera.rotation.z = t * -0.05
-}
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
 
-document.body.onscroll = moveCamera
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+
+// Scroll Animation
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    moon.rotation.x += 0.05;
+    moon.rotation.y += 0.075;
+    moon.rotation.z += 0.05;
+  
+    saturn.rotation.y += 0.01;
+    saturn.rotation.z += 0.01;
+  
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+  }
+  
+  document.body.onscroll = moveCamera;
+  moveCamera();
 
 
 function animate() {
     requestAnimationFrame(animate);
 
+    saturnRing.rotation.x += 0.0035
+    saturnRing.rotation.y += 0.0012
     saturnRing.rotation.z += 0.0035
+
+    saturn.rotation.x += 0.0035
 
     renderer.render(scene, camera)
 
-    controls.update()
+    // controls.update()
 }
 
 animate()
