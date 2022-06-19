2a319c46484aee75c8b2b5692b383c3f9280dfe7
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-06-19 13:04:05 -0300

    feat: adds controls to canvas

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -29,7 +30,7 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper, gridHelper)
 
-
+const controls = new OrbitControls(camera, renderer.domElement)
 
 function animate() {
     requestAnimationFrame(animate);
@@ -37,6 +38,8 @@ function animate() {
     torus.rotation.z += 0.0035
 
     renderer.render(scene, camera)
+
+    controls.update()
 }
 
 animate()
